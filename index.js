faca33ee1cf04c39f4f35858703f639661168c02
Tyoma <raskolnikoffweb@mail.ru>
2024-04-30 19:01:17 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+import { Grid } from "./grid.js";
+import { Tile } from "./tile.js";
+
+const gameBoard = document.getElementById("game-board");
+
+const grid = new Grid(gameBoard);
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
+grid.getRandomEmptyCell().linkTile(new Tile(gameBoard));
+setupInputOnce()
+
+function setupInputOnce(){
+  window.addEventListener("keydown", handleInput, {once:true})
+}
+
+function handleInput(event){
+  switch (event.key) {
+    case "ArrowUp":
+      moveUp()
+      break;
+
+    case "ArrowDown":
+      moveDown();
+      break;
+
+    case "ArrowLeft":
+      break;
+
+    case "ArrowRight":
+      break;
+
+    default:
+      setupInputOnce()
+      return;
+  }
+}
+
+function moveUp() {
+  slideTiles(grid.cellsGroupedByColumn);
+}
+
+function moveDown() {
+  slideTiles(grid.cellsGroupedByReversedColumn);
+}
+
+function slideTiles(groupedCells){
+  groupedCells.forEach(group => slideTilesInGroup(group));
+
+  grid.cells.forEach(cell => {
+    cell.hasTileForMerge() && cell.mergeTiles();
+  })
+}
+
+function slideTilesInGroup(group){
+  for(let i = 1;i < group.length; i++){
+    if(group[i].isEmpty()) {
+      continue
+    }
+
+    const cellWithTile = group[i];
+
+    let targetCell;
+    let j = i - 1;
+    while(j >= 0 && group[j].canAccept(cellWithTile.linkedTile)) {
+      targetCell = group[j];
+      j--;
+    }
+
+    if (!targetCell) {
+      continue;
+    }
+
+    if (targetCell.isEmpty()) {
+      targetCell.linkTile(cellWithTile.linkedTile);
+    } else {
+      targetCell.linkTileForMerge(cellWithTile.linkedTile);
+    }
+
+    cellWithTile.unlinkTile();
+  }
+}
